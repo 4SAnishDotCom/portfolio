db07b6e3e58d08dc4ea1c6dbe7a6ee19ada6bb2e
4SAnishDotCom <anishksenthil@gmail.com>
2019-11-19 16:45:39 +0000

    My first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my Links [AnishKumar Senthil period 2]</p>'; ?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <!-- Please make sure to change all the #'s to the proper link names-->
+      <li><a href="Senthil_AboutMe.html">AboutMe.html</a></li></br>
+      <li><a href="Senthil_ContactMe.html">Projects.html</a></li></br>
+      <li><a href="Senthil_Projects.html">Resume.html</a></li></br>
+      <li><a href="Senthil_Resume.html">ContactMe.html</a></li></br>
+    <ul>
+  </body>
+</html>
